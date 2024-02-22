130c757942c6f756ddca2f79e1369f481017f97c
DaGo85 <davidgoe85@gmail.com>
2024-02-22 16:18:40 +0100

    adjusted more ts and removed themecontext

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,13 @@ import App from "./App.tsx";
 import "./index.scss";
 import AuthProvider from "./context/AuthProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
